aa33620bc9bc677fd3a3959fdbcaf573191f6a59
handgun <dungphase@gmail.com>
2023-10-29 19:19:08 +0700

    update new

--- server.js
+++ server.js
@@ -1,32 +1,50 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+var users = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+io.on("connection", (socket) => {
+    console.log(`${socket.id} da ket noi den server`);
+    socket.on("register", data => {
+        if (users.indexOf(data) >= 0) {
+            socket.emit("failed", "username already in use!")
+        } else {
+            users.push(data)
+            // socket.username = data;
+            socket.emit("successfull", data)
+            io.emit("list useronline", users)
+        }
+    })
 
-    // Handle chat messages
+    socket.on("logout", (data) => {
+        users.splice(users.indexOf(data), 1)
+        socket.emit("logout complete")
+        io.emit("list useronline", users)
+    })
 
-    socket.on('chat message', data => {
-        io.emit('response message', data); // Broadcast the message to all connected clients
-    });
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-    // Handle user disconnection
+    socket.on("message", data => {
+        io.emit("message", data)
+    })
 
-});
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} da ngat ket noi`);
+    })
+})
 
-
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+httpServer.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+})
